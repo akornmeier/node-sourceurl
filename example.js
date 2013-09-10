9bd0d25e126bc50f93634d3b05cbb476a6a0e52b
Stephen Mathieson <smath23@gmail.com>
2013-09-10 11:53:38 -0400

    worthless context

--- example.js
+++ example.js
@@ -14,7 +14,7 @@ var js = [
 var fn = sourceURL(js, [ 'message' ], 'test.js');
 
 try {
-  vm.runInNewContext(fn + '.call(null, "it works")', { console: console });
+  vm.runInNewContext(fn + '.call(null, "it works")', {});
 } catch (err) {
   assert(~err.stack.indexOf('(test.js:2:7)'));
 }
